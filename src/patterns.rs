be04f4f72f25119cf0d5b92244921b41a2d32ca0
Christopher Wells <cwellsny@nycap.rr.com>
2018-08-05 14:57:36 -0700

    Document GOLPattern trait

--- src/patterns.rs
+++ src/patterns.rs
@@ -1,7 +1,38 @@
 extern crate ndarray;
 
+/// A pattern for Conway's Game of Life that can be written to a game board.
+///
+/// ```
+/// extern crate gol;
+/// extern crate ndarray;
+///
+/// // Pattern that draws a 2x2 block
+/// struct Block {}
+///
+/// impl gol::GOLPattern for Block {
+///     fn place(&self, read: &mut ndarray::Array2<bool>, i: usize, j: usize) {
+///         read[[i, j]] = true;
+///         read[[i + 1, j]] = true;
+///         read[[i, j + 1]] = true;
+///         read[[i + 1, j + 1]] = true;
+///     }
+///
+///     fn get_dims(&self) -> (usize, usize) {
+///         (2, 2)
+///     }
+/// }
+/// ```
 pub trait GOLPattern {
+    /// Places the pattern onto the given game board starting at the given
+    /// position.
+    ///
+    /// This function must not change any values outside the region of the
+    /// given starting position plus the pattern dimensions returned by
+    /// `get_dims`.
     fn place(&self, read: &mut ndarray::Array2<bool>, i: usize, j: usize);
+
+    /// Returns the dimensions of the region that the pattern will be written
+    /// to.
     fn get_dims(&self) -> (usize, usize);
 }
 
